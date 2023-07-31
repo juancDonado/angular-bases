0fb4d36900047465babdd9045a42c6cb7ff59d21
Juan Carlos Donado Vanegas <jdonadovanegas@gmail.com>
2023-07-31 17:18:36 -0500

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="reset()">reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent implements OnInit {
+
+  public counter: number = 10;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  public increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  reset(): void {
+    this.counter = 10
+  }
+
+}
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalidedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+
+  changeHero(): void {
+    // throw 'Metodo no implementado';
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  resetForm(): void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+
+}
